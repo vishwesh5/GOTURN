0f49bfc98464aff5461193fedf301bc7798bea37
vishwesh5 <vishweshshrimali5@gmail.com>
2018-09-06 21:15:21 +0530

    Update download_trained_model.sh
    cs.stanford.edu certificate is not verifiable

--- scripts/download_trained_model.sh
+++ scripts/download_trained_model.sh
@@ -2,5 +2,5 @@
 
 mkdir -p nets/models/pretrained_model 
 cd nets/models/pretrained_model
-wget http://cs.stanford.edu/people/davheld/public/GOTURN/trained_model/tracker.caffemodel
+wget http://cs.stanford.edu/people/davheld/public/GOTURN/trained_model/tracker.caffemodel --no-check-certificate
 cd ../../../
